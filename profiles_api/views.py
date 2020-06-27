b51f4e326b1e13959f196729b9f1ec11bd80060f
Akila Mendis <akila@oneit.com.au>
2020-06-27 19:39:52 +0530

    Added HelloSerializer class and POST function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -13,7 +16,21 @@ class HelloApiView(APIView):
             'Gives you the most control over your application logic',
             'Is mapped manually to URLs'
         ]
-        print('*********request********')
+        print('*********get request********')
         print(str(request))
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
-    
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        print('*********post request.data********')
+        print(str(request.data))
+        serializer = self.serializer_class(data=request.data)
+        print('*********post serializer********')
+        print(str(serializer))
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
