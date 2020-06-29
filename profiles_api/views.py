cf38178b883dc887fa6ff93013710eb604e7c00d
Akila Mendis <akila@oneit.com.au>
2020-06-29 18:53:18 +0530

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,11 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
-from  rest_framework import filters
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
-from  profiles_api import permissions
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -71,7 +73,7 @@ class HelloViewSet(viewsets.ViewSet):
     def create(self, request):
         """Create a new hello message"""
         serializer = self.serializer_class(data=request.data)
-        
+
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Hello {name}!'
@@ -79,19 +81,19 @@ class HelloViewSet(viewsets.ViewSet):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def retrieve(self,request,pk=None):
+    def retrieve(self, request, pk=None):
         """Handle getting an object by its ID"""
         return Response({'http_method': 'GET'})
 
-    def update(self,request,pk=None):
+    def update(self, request, pk=None):
         """Handle updating an object"""
         return Response({'http_method': 'PUT'})
 
-    def partial_update(self,request,pk=None):
+    def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
         return Response({'http_method': 'PATCH'})
 
-    def destroy(self,request,pk=None):
+    def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
 
@@ -102,5 +104,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-    filter_backends = (filters.SearchFilter, )
+    filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
